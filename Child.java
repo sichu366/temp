b4f24dd734a5fda60878566a2df5d62c30fe3b5e
sichu366 <sichu@ucsc.edu>
2018-05-11 00:39:33 -0700

    modified Child.java new .gitignore

--- Child.java
+++ Child.java
@@ -1,17 +1,29 @@
 class Child extends Parent {
-  int i;
+  //int i;
   int j;
 
-  public int callI(){
+  public int getSuperI(){
     return super.i;
   }
+
+  public void setI(int i) {
+    this.i = i;
+  }
+
+  public int getI(){
+    return this.i;
+  }
+
+
 
   public static void main(String[] args) {
     //System.out.println("wangshen");
 
     Child c = new Child();
     //System.out.println("super i = " + ((Parent)c).i);
-    System.out.println("super i = " + c.callI());
-    System.out.println("child i = " + c.i);
+    System.out.println("super i = " + c.getSuperI());
+    c.setI(4);
+    System.out.println("child i = " + c.getI());
+    System.out.println("super i = " + c.getSuperI());
   }
 }

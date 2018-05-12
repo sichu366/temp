2c37b64de50d31270334d371ff8ec3477c1472ed
sichu366 <sichu@ucsc.edu>
2018-05-12 16:44:03 -0700

    superclass and subclass have same method

--- Child.java
+++ Child.java
@@ -14,16 +14,34 @@ class Child extends Parent {
     return this.i;
   }
 
+  public void printAge(){
+    System.out.println("Siying is 0 years old");
+  }
+
+  public void getSuperPrint(){
+    super.printAge();
+  }
+
+  public void runSuperExe(){
+    super.exe();
+  }
+
+
+
 
 
   public static void main(String[] args) {
     //System.out.println("wangshen");
 
     Child c = new Child();
+    Parent p = new Child();
+    p.exe();
     //System.out.println("super i = " + ((Parent)c).i);
     System.out.println("super i = " + c.getSuperI());
     c.setI(4);
     System.out.println("child i = " + c.getI());
     System.out.println("super i = " + c.getSuperI());
+    c.getSuperPrint();
+    c.runSuperExe();
   }
 }
--- Parent.java
+++ Parent.java
@@ -1,3 +1,11 @@
 class Parent {
   int i = 1;
+
+  public void printAge(){
+    System.out.println("Shen is 0 years old");
+  }
+
+  public void exe(){
+    this.printAge();
+  }
 }
